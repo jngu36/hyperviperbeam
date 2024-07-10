ffcd5fb1951eca2767637f7cf065ec95c8dd745e
jngu36 <jnshin36@gmail.com>
2024-07-10 18:52:44 -0400

    updated names and hopefully its all viper bar

--- ViperBar/Windows/ViperConfig.cs
+++ ViperBar/Windows/ViperConfig.cs
@@ -1,17 +1,3 @@
-/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SamplePlugin.Windows
-{
-    internal class ViperBar
-    {
-    }
-}
-*/
-
 using System;
 using System.Linq;
 using System.Numerics;
